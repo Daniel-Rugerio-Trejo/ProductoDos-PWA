ea3f863bba82ac458a9da86610ed3c29265e084f
Daniel-Rugerio-Trejo <Daniel_Rugerio_Trejo@hotmail.com>
2022-11-25 07:53:33 -0600

    second commit

---
+++ sw.js
@@ -0,0 +1,45 @@
+var nombreCache = 'uno';
+self.addEventListener(
+    'install',
+    function(event){
+        event.waitUntil(
+            caches.open(nombreCache)
+            .then(
+                function(cache){
+                    cache.addAll(
+                        [
+                            'algo.js',
+                            'w3.css',
+                            'index.html',
+                            'manifest.json',
+                            './iconos/homescreen.png',
+                            './iconos/homescreen144.png',
+                            './iconos/homescreen192.png'
+                        ]
+                    );
+                }
+            )
+        );
+    }
+);
+
+self.addEventListener(
+    'fetch', 
+    function(event) {
+        event.respondWith( 
+            caches.match(event.request)
+            .then(
+                function(respuesta){
+                    if(respuesta){ 
+                        console.log('La respuesta proviene del cache.');
+                        return respuesta; 
+                    }
+                    else{
+                        console.log('La respuesta no proviene del cache.');
+                        return fetch(event.request);
+                    }
+                }
+            ) 
+        );
+    }
+); 
